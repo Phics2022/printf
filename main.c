111912f62aec8216a7b5fc97ded2dec32a0d93d0
Akar Excel <91137029+AkarExcel@users.noreply.github.com>
2023-03-27 18:07:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
 
+
+int main(void)
+{
+	char c = '!';
+	char *str = "Hello, world";
+	int count = _printf("Printing a character: %c\nPrinting a string: %s\nPrinting a percent sign: %%\n", c, str);
+	printf("Printed %d characters\n", count);
+	return 0;
+}
