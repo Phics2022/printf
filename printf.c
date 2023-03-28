2be71536380de4b25635767b04be4578180a7f8b
Oladipo <phaymos2022@gmail.com>
2023-03-28 13:56:51 -0700

    almost done

--- printf.c
+++ printf.c
@@ -1,48 +1,49 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
+
 /**
- *_printf - create a printf function
- *@format: format
+ * _printf - prints formatted output to stdout
+ * @format: format string containing zero or more directives
  *
- *Return: characters count
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
-const char *ptr = format;
-int num_cont = 0;
-va_list args;
-va_start(args, format);
-if (ptr != NULL)
-{
-while (*ptr)
-{
-if (*ptr == '%')
-{
-ptr++;
-switch (*ptr)
-{
-case 'c':
-case_c((char)va_arg(args, int), num_cont);
-break;
-case 's':
-case_s(va_arg(args, const char*), num_cont);
-break;
-case '%':
-case_pers(num_cont);
-break;
-default:
-def(ptr, num_cont);
-break;
-}
-}
-else
-write(1, ptr, 1);
-num_cont++;
-ptr++;
-}
-va_end(args);
-}
-return (num_cont);
+	int count = -1;
+
+	if (format != NULL)
+	{
+		int i;
+		va_list ar_list;
+		int (*o)(va_list);
+
+		va_start(ar_list, format);
+
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+
+		count = 0;
+
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					o = get_func(format[i + 1]);
+					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
+					i++;
+				}
+			}
+			else
+				count += _putchar(format[i]);
+		}
+		va_end(ar_list);
+	}
+
+	return (count);
 }
